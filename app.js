e18727886c7e47f06f664766acfe5a8335f7a06c
Eric Garza <Eric_Garza@intuit.com>
2020-02-15 09:41:37 -0800

    Read input data from the UI

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 var budgetController = (function() {
-  var x = 23;
 
-  var add = function(a) {
-    return x + a + y;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
 })();
 
 var UIController = (function() {
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn'
+  }
 
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, // will be either inc or exp 
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      }
+    },
+
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  };
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
+  var DOM = UICtrl.getDOMStrings();
 
-  return {
-    anotherPublic: function() {
-      console.log(z);
+  var ctrlAddItem = function () {
+    var input = UICtrl.getInput();
+    console.log(input);
+
+    // 2. Add item to the budget controller
+
+    // 3. Add new item to user interface
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the user interface
+  };
+
+  document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
     }
-  }
+  });
 })(budgetController, UIController);
 
