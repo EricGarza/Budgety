7a5503f0f10f3383b69e789cbb43cd8a861e1a46
Eric Garza <Eric_Garza@intuit.com>
2020-02-15 12:07:35 -0800

    Implement add Income or Expense item functionality

--- app.js
+++ app.js
@@ -21,6 +21,32 @@ var budgetController = (function() {
       inc: 0
     }
   };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      data.allItems[type].push(newItem);
+
+      return newItem;
+    },
+    
+    testing: function() {
+      console.log(data);
+    }
+  };
 })();
 
 var UIController = (function() {
@@ -28,7 +54,9 @@ var UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputButton: '.add__btn'
+    inputButton: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   }
 
   return {
@@ -42,6 +70,24 @@ var UIController = (function() {
 
     getDOMStrings: function() {
       return DOMStrings;
+    },
+
+    addListItem: function(obj, type) {
+      var html, newHtml, element;
+
+      if (type === 'inc') {
+        element = DOMStrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+      } else if (type === 'exp') {
+        element = DOMStrings.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+      }
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     }
   };
 })();
@@ -60,11 +106,13 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
-    var input = UICtrl.getInput();
+    var input, newItem;
 
-    // 2. Add item to the budget controller
+    input = UICtrl.getInput();
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add new item to user interface
+    UICtrl.addListItem(newItem, input.type);
 
     // 4. Calculate the budget
 
